bf828c2d7c67b930aa6fbc9035c48a7094674c36
Fauzan Lubis <7534613+fauh45@users.noreply.github.com>
2022-05-16 09:47:32 +0000

    Edit Chaikin Algorithm Typings

--- src/lib/chaikin.ts
+++ src/lib/chaikin.ts
@@ -1,17 +1,20 @@
 /**
  * From https://github.com/mattdesl/vec2-copy/blob/master/index.js
  */
-const copy = (out: any[], a: any[]) => {
+const copy = <T>(out: [T, T], a: [T, T]): [T, T] => {
   out[0] = a[0];
   out[1] = a[1];
+
   return out;
 };
 
 /**
  * From https://github.com/Jam3/chaikin-smooth/blob/master/index.js
  */
-export const chaikinSmooth = (input: [number, number][]) => {
-  let output = [];
+export const chaikinSmooth = (
+  input: [number, number][]
+): [number, number][] => {
+  let output: [number, number][] = [];
 
   if (input.length > 0) output.push(copy([0, 0], input[0]));
   for (var i = 0; i < input.length - 1; i++) {
@@ -22,11 +25,19 @@ export const chaikinSmooth = (input: [number, number][]) => {
       p1x = p1[0],
       p1y = p1[1];
 
-    var Q = [0.75 * p0x + 0.25 * p1x, 0.75 * p0y + 0.25 * p1y];
-    var R = [0.25 * p0x + 0.75 * p1x, 0.25 * p0y + 0.75 * p1y];
+    var Q: [number, number] = [
+      0.75 * p0x + 0.25 * p1x,
+      0.75 * p0y + 0.25 * p1y,
+    ];
+    var R: [number, number] = [
+      0.25 * p0x + 0.75 * p1x,
+      0.25 * p0y + 0.75 * p1y,
+    ];
     output.push(Q);
     output.push(R);
   }
-  if (input.length > 1) output.push(copy([0, 0], input[input.length - 1]));
+  if (input.length > 1)
+    output.push(copy<number>([0, 0], input[input.length - 1]));
+
   return output;
 };
